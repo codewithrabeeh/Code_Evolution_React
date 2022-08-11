66f28b21ec8d281946f6190ac613b2e6e8e6f43f
codewithrabeeh <real.ray48@gmail.com>
2022-08-11 21:07:49 +0530

    v1

---
+++ 4-Props/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, {Component}  from "react";
+
+class Welcome extends Component{
+    render(){
+        return <h2>Class Component Want to Say 'Hi'</h2>
+    }
+}
+
+export default Welcome
